1d863924ada3ebf5b2b6e3452fb84cfa6f68920e
sabareeshgovind <56748663+sabareeshgovind@users.noreply.github.com>
2020-06-26 16:14:59 -0400

    Security cfg, roles and dependency updated

---
+++ src/main/java/com/sabareesh/springsecurity/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,9 @@
+package com.sabareesh.springsecurity.config;
+
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer 
+						extends AbstractSecurityWebApplicationInitializer {
+
+}
